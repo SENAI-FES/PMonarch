25a050963989da989fea0a4194802bb6eefa83de
eduardo_ciepanski <eduardo_ciepanski@SNSOO1EP01VM125.saojose.sc.senai.br>
2015-06-22 20:02:34 -0300

    criado a classe Contato para ser usado pela classe Empresa

---
+++ PMonarchyFiles/src/entity/Contato.java
@@ -0,0 +1,55 @@
+
+package entity;
+
+/**
+ *
+ * @author eduardo_ciepanski
+ */
+public class Contato {
+    private String nome;
+    private String cargo;
+    private String telefone;
+    private String ramal;
+    private String email;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getRamal() {
+        return ramal;
+    }
+
+    public void setRamal(String ramal) {
+        this.ramal = ramal;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+}

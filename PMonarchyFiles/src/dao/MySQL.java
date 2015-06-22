f9c167afd950e8c4d598b290b172826728613227
ricardo.nene <ricardo.nene@SNSOO1EP01VM176.saojose.sc.senai.br>
2015-06-22 19:04:23 -0300

    Arquivo de conexão com o banco

--- PMonarchyFiles/src/dao/MySQL.java
+++ PMonarchyFiles/src/dao/MySQL.java
@@ -6,10 +6,33 @@
 
 package dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author ricardo.nene
  */
 public class MySQL {
     
+      public Connection getConnection() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+
+            String nomeServidor = "localhost";
+            String database = "dbMonarchyFiles";
+            String url = "jdbc:mysql://" + nomeServidor + "/" + database;
+            String username = "root";
+            String password = "";
+
+            return DriverManager.getConnection(url, username, password);
+        } catch (ClassNotFoundException e) {
+            System.out.println("O driver expecificado nao foi encontrado.");
+            return null;
+        } catch (SQLException e) {
+            System.out.println("Nao foi possivel conectar ao Banco de Dados.");
+            return null;
+        }
+    }
 }

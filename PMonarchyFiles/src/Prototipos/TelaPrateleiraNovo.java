093e74d583cf8fe5d4245fde6200d9fa055b0f5b
Matheus_santos_fraga <Matheus_santos_fraga@SNSOO1EP01VM023.saojose.sc.senai.br>
2015-06-22 20:52:05 -0300

    CaixaDAO e Tela ajeitada

--- PMonarchyFiles/src/Prototipos/TelaPrateleiraNovo.java
+++ PMonarchyFiles/src/Prototipos/TelaPrateleiraNovo.java
@@ -1,12 +1,21 @@
 package Prototipos;
 
+import dao.CaixaDAO;
+import entity.Caixa;
+import javax.swing.JOptionPane;
+
 public class TelaPrateleiraNovo extends javax.swing.JDialog {
 
     public TelaPrateleiraNovo(java.awt.Frame parent, boolean modal) {
         super(parent, modal);
         initComponents();
         this.setLocationRelativeTo(null);
-      
+    }
+    public TelaPrateleiraNovo(java.awt.Frame parent, boolean modal, boolean novo) {
+        super(parent, modal);
+        initComponents();
+        this.setLocationRelativeTo(null);
+        this.novo = novo;
     }
 
     @SuppressWarnings("unchecked")
@@ -16,29 +25,35 @@ public class TelaPrateleiraNovo extends javax.swing.JDialog {
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
-        txtColuna = new javax.swing.JTextField();
+        txtAndar = new javax.swing.JTextField();
         txtRua = new javax.swing.JTextField();
-        txtAndar = new javax.swing.JFormattedTextField();
+        txtColunaInicio = new javax.swing.JFormattedTextField();
         btnSalvar = new javax.swing.JButton();
         btnCancelar = new javax.swing.JButton();
         jLabel4 = new javax.swing.JLabel();
         txtEstante = new javax.swing.JTextField();
-        ckbPreencherEstante = new javax.swing.JCheckBox();
+        txtColunaFim = new javax.swing.JFormattedTextField();
+        jLabel5 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setTitle("Cadastro de Prateleira");
 
         jLabel1.setText("Rua:");
 
-        jLabel2.setText("Coluna:");
+        jLabel2.setText("Andar:");
 
-        jLabel3.setText("Andar:");
+        jLabel3.setText("Coluna:");
 
-        txtAndar.setFormatterFactory(new javax.swing.text.DefaultFormatterFactory(new javax.swing.text.NumberFormatter(java.text.NumberFormat.getIntegerInstance())));
+        txtColunaInicio.setFormatterFactory(new javax.swing.text.DefaultFormatterFactory(new javax.swing.text.NumberFormatter(java.text.NumberFormat.getIntegerInstance())));
 
         btnSalvar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/OK.png"))); // NOI18N
         btnSalvar.setText("Salvar");
         btnSalvar.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        btnSalvar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnSalvarActionPerformed(evt);
+            }
+        });
 
         btnCancelar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/Cancel.png"))); // NOI18N
         btnCancelar.setText("Cancelar");
@@ -51,7 +66,9 @@ public class TelaPrateleiraNovo extends javax.swing.JDialog {
 
         jLabel4.setText("Estante:");
 
-        ckbPreencherEstante.setText("Utilizar toda a estante");
+        txtColunaFim.setFormatterFactory(new javax.swing.text.DefaultFormatterFactory(new javax.swing.text.NumberFormatter(java.text.NumberFormat.getIntegerInstance())));
+
+        jLabel5.setText("até");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -60,6 +77,10 @@ public class TelaPrateleiraNovo extends javax.swing.JDialog {
             .addGroup(layout.createSequentialGroup()
                 .addGap(103, 103, 103)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(btnSalvar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(btnCancelar))
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                             .addComponent(jLabel3)
@@ -69,17 +90,16 @@ public class TelaPrateleiraNovo extends javax.swing.JDialog {
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(txtAndar)
-                            .addComponent(txtColuna)
                             .addComponent(txtRua)
-                            .addComponent(txtEstante)))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addGap(0, 0, Short.MAX_VALUE)
-                        .addComponent(ckbPreencherEstante)
-                        .addGap(7, 7, 7))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addComponent(btnSalvar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(btnCancelar)))
+                            .addComponent(txtEstante)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(txtColunaInicio, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(jLabel5)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(txtColunaFim, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(0, 0, Short.MAX_VALUE)))
+                        .addGap(7, 7, 7)))
                 .addGap(89, 89, 89))
         );
         layout.setVerticalGroup(
@@ -96,14 +116,14 @@ public class TelaPrateleiraNovo extends javax.swing.JDialog {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel2)
-                    .addComponent(txtColuna, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(txtAndar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel3)
-                    .addComponent(txtAndar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(ckbPreencherEstante)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                    .addComponent(txtColunaInicio, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(txtColunaFim, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel5))
+                .addGap(20, 20, 20)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(btnSalvar)
                     .addComponent(btnCancelar))
@@ -114,8 +134,27 @@ public class TelaPrateleiraNovo extends javax.swing.JDialog {
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnCancelarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCancelarActionPerformed
-        this.dispose();        
+        this.dispose();
     }//GEN-LAST:event_btnCancelarActionPerformed
+
+    private void btnSalvarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSalvarActionPerformed
+        Caixa caixa = new Caixa();
+        caixa.setAndar(txtAndar.getText());
+        caixa.setRua(txtRua.getText());
+        caixa.setEstante(txtEstante.getText());
+        caixa.setColunaInicio(txtColunaInicio.getText());
+        caixa.setColunaFim(txtColunaFim.getText());
+        CaixaDAO dao = new CaixaDAO();
+        if (novo) {
+            if (dao.insert(caixa)) {
+                JOptionPane.showMessageDialog(null, "sucez0");
+            }
+        } else {
+
+        }
+        System.out.println(caixa.getAndar() + "\n" + caixa.getRua() + "\n" + caixa.getEstante()
+                + "\n" + caixa.getColunaInicio() + "\n" + caixa.getColunaFim());
+    }//GEN-LAST:event_btnSalvarActionPerformed
 
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
@@ -128,16 +167,21 @@ public class TelaPrateleiraNovo extends javax.swing.JDialog {
                 if ("Nimbus".equals(info.getName())) {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
                     break;
+
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(TelaPrateleiraNovo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TelaPrateleiraNovo.class
+                    .getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(TelaPrateleiraNovo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TelaPrateleiraNovo.class
+                    .getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(TelaPrateleiraNovo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TelaPrateleiraNovo.class
+                    .getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(TelaPrateleiraNovo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TelaPrateleiraNovo.class
+                    .getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
@@ -159,14 +203,16 @@ public class TelaPrateleiraNovo extends javax.swing.JDialog {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnCancelar;
     private javax.swing.JButton btnSalvar;
-    private javax.swing.JCheckBox ckbPreencherEstante;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
-    private javax.swing.JFormattedTextField txtAndar;
-    private javax.swing.JTextField txtColuna;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JTextField txtAndar;
+    private javax.swing.JFormattedTextField txtColunaFim;
+    private javax.swing.JFormattedTextField txtColunaInicio;
     private javax.swing.JTextField txtEstante;
     private javax.swing.JTextField txtRua;
     // End of variables declaration//GEN-END:variables
+    boolean novo;
 }
---
+++ PMonarchyFiles/src/dao/CaixaDAO.java
@@ -0,0 +1,180 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+    
+    package dao;
+
+import entity.Caixa;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Matheus_santos_fraga
+ */
+public class CaixaDAO extends MySQL {
+
+    public boolean insert(Caixa caixa) {
+        Connection c = this.getConnection();
+        try {
+            PreparedStatement ps
+                    = c.prepareStatement("INSERT INTO caixa "
+                            + "( Rua, Estante, Coluna, Andar )  "
+                            + "VALUES ( ?, ?, ?, ?)");
+            ps.setString(1, caixa.getRua());
+            ps.setString(2, caixa.getEstante());
+            ps.setString(3, caixa.getColuna());
+            ps.setString(4, caixa.getAndar());
+            
+            ps.execute();
+            ps.close();
+            return true;
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } finally {
+            try {
+                c.close();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+    public boolean update(Caixa caixa) {
+        Connection c = this.getConnection();
+        try {
+            PreparedStatement ps = c.prepareStatement("UPDATE caixa "
+                    + " SET rua = ?, estante = ?, coluna = ?, andar = ? "
+                    + " WHERE idCaixa = ?");
+            ps.setString(1, caixa.getRua());
+            ps.setString(2, caixa.getColuna());
+            ps.setString(3, caixa.getEstante());
+            ps.setString(4, caixa.getAndar());
+           
+            ps.execute();
+
+            ps.close();
+            return true;
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } finally {
+            try {
+                c.close();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+        return false;
+    }
+    /*
+
+    public boolean delete(int id) {
+        Connection c = this.getConnection();
+        try {
+            PreparedStatement ps
+                    = c.prepareStatement("DELETE FROM funcionario "
+                            + "WHERE id_funcionario = ?");
+            ps.setInt(1, id);
+
+            ps.execute();
+
+            ps.close();
+            return true;
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } finally {
+            try {
+                c.close();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+    public List<Funcionario> listarFuncionarios() {
+        List<Funcionario> lista = new ArrayList<Funcionario>();
+        Connection c = this.getConnection();
+        try {
+            PreparedStatement ps
+                    = c.prepareStatement("SELECT id_funcionario, matricula, nome, rua, numero, bairro,"
+                            + " cep, uf, fone_residencial, fone_celular, salario, setor, funcao "
+                            + "FROM funcionario");
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+
+                FContratado funcionario = new FContratado();
+                funcionario.setId_funcionario(rs.getInt("id_funcionario"));
+                funcionario.setMatricula(rs.getInt("Matricula"));
+                funcionario.setNome(rs.getString("Nome"));
+                funcionario.setRua(rs.getString("Rua"));
+                funcionario.setNumero(rs.getInt("Numero"));
+                funcionario.setBairro(rs.getString("Bairro"));
+                funcionario.setCep(rs.getString("Cep"));
+                funcionario.setUf(rs.getString("Uf"));
+                funcionario.setResidencial(rs.getString("fone_residencial"));
+                funcionario.setCelular(rs.getString("fone_celular"));
+                funcionario.setSalario(rs.getDouble("salario"));
+                funcionario.setSetor(EnumSetor.ADMINISTRATIVO.getEnumSetorPorCodigo(rs.getInt("setor")));
+                funcionario.setFuncao(EnumFuncao.ANALISTA.getEnumPorCodigo(rs.getInt("funcao")));
+
+                lista.add(funcionario);
+            }
+            rs.close();
+            ps.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } finally {
+            try {
+                c.close();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+        return lista;
+    }
+
+    public FContratado getFuncionarioById(int id) {
+        Connection c = this.getConnection();
+        FContratado funcionario = null;
+        try {
+            PreparedStatement ps = c.prepareStatement("SELECT id_funcionario, "
+                    + "matricula, nome "
+                    + "FROM funcionario WHERE id_funcionario = ?");
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+
+                funcionario = new FContratado();
+                funcionario.setId_funcionario(rs.getInt("id_funcionario"));
+                funcionario.setMatricula(rs.getInt("matricula"));
+                funcionario.setNome(rs.getString("nome"));
+                funcionario.setRua(rs.getString("rua"));
+                funcionario.setNumero(rs.getInt("numero"));
+
+            }
+            rs.close();
+            ps.close();
+            return funcionario;
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } finally {
+            try {
+                c.close();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+        return null;
+    }*/
+    
+}

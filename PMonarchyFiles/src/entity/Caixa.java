92d16709ec723ee00356ee4eca2c30ef32aad540
Matheus_santos_fraga <Matheus_santos_fraga@SNSOO1EP01VM023.saojose.sc.senai.br>
2015-06-22 20:46:09 -0300

    Caixa ajeitado

--- PMonarchyFiles/src/entity/Caixa.java
+++ PMonarchyFiles/src/entity/Caixa.java
@@ -1,16 +1,5 @@
 package entity;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
-/**
- *
- * @author gabriel_pereira
- */
 public class Caixa {
 
     private int idCaixa;

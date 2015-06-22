1f73206baf88651e66f7cf2a37f868a29adb3e56
ricardo_jose_santana <ricardo_jose_santana@SNSOO1EP01VM162.saojose.sc.senai.br>
2015-06-22 19:45:37 -0300

    Criar  Marcador e MarcadorDAO

---
+++ PMonarchyFiles/src/entity/Marcador.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package entity;
+
+/**
+ *
+ * @author ricardo_jose_santana
+ */
+public class Marcador {
+    
+    private int idMarcador;
+    private String Descricao;
+
+    public int getIdMarcador() {
+        return idMarcador;
+    }
+
+    public void setIdMarcador(int idMarcador) {
+        this.idMarcador = idMarcador;
+    }
+
+    public String getDescricao() {
+        return Descricao;
+    }
+
+    public void setDescricao(String Descricao) {
+        this.Descricao = Descricao;
+    }
+    
+    
+    
+    
+}

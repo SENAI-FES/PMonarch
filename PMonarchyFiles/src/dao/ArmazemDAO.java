f86402d4604197683e6fc9a5f048f8cdb64f9b5c
Matheus_santos_fraga <Matheus_santos_fraga@SNSOO1EP01VM023.saojose.sc.senai.br>
2015-06-22 21:32:29 -0300

    Armazem DAO refeito

--- PMonarchyFiles/src/dao/ArmazemDAO.java
+++ PMonarchyFiles/src/dao/ArmazemDAO.java
@@ -8,6 +8,7 @@
     
     package dao;
 
+import entity.Armazem;
 import entity.Caixa;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -22,17 +23,17 @@ import java.util.List;
  */
 public class ArmazemDAO extends MySQL {
 
-    public boolean insert(Caixa caixa) {
+    public boolean insert(Armazem armazem) {
         Connection c = this.getConnection();
         try {
             PreparedStatement ps
                     = c.prepareStatement("INSERT INTO caixa "
                             + "( Rua, Estante, Coluna, Andar )  "
                             + "VALUES ( ?, ?, ?, ?)");
-            ps.setString(1, caixa.getRua());
-            ps.setString(2, caixa.getEstante());
-            ps.setString(3, caixa.getColuna());
-            ps.setString(4, caixa.getAndar());
+            ps.setString(1, armazem.getRua());
+            ps.setString(2, armazem.getEstante());
+            ps.setString(3, armazem.getColuna());
+            ps.setString(4, armazem.getAndar());
 
             ps.execute();
             ps.close();
@@ -50,16 +51,16 @@ public class ArmazemDAO extends MySQL {
         return false;
     }
 
-    public boolean update(Caixa caixa) {
+    public boolean update(Armazem armazem) {
         Connection c = this.getConnection();
         try {
-            PreparedStatement ps = c.prepareStatement("UPDATE caixa "
+            PreparedStatement ps = c.prepareStatement("UPDATE armazem "
                     + " SET rua = ?, estante = ?, coluna = ?, andar = ? "
-                    + " WHERE idCaixa = ?");
-            ps.setString(1, caixa.getRua());
-            ps.setString(2, caixa.getColuna());
-            ps.setString(3, caixa.getEstante());
-            ps.setString(4, caixa.getAndar());
+                    + " WHERE idArmazem = ?");
+            ps.setString(1, armazem.getRua());
+            ps.setString(2, armazem.getColuna());
+            ps.setString(3, armazem.getEstante());
+            ps.setString(4, armazem.getAndar());
 
             ps.execute();
 
@@ -78,13 +79,13 @@ public class ArmazemDAO extends MySQL {
         return false;
     }
 
-    public boolean delete(int idCaixa) {
+    public boolean delete(int idArmazem) {
         Connection c = this.getConnection();
         try {
             PreparedStatement ps
-                    = c.prepareStatement("DELETE FROM caixa "
-                            + "WHERE idCaixa = ?");
-            ps.setInt(1, idCaixa);
+                    = c.prepareStatement("DELETE FROM armazem "
+                            + "WHERE idArmazem = ?");
+            ps.setInt(1, idArmazem);
 
             ps.execute();
 
@@ -103,23 +104,23 @@ public class ArmazemDAO extends MySQL {
         return false;
     }
 
-    public List<Caixa> listarCaixas() {
-        List<Caixa> lista = new ArrayList<>();
+    public List<Armazem> listarArmazem() {
+        List<Armazem> lista = new ArrayList<>();
         Connection c = this.getConnection();
         try {
             PreparedStatement ps
                     = c.prepareStatement("SELECT rua, estante, coluna, andar"
-                            + "FROM caixa");
+                            + "FROM armazem");
             ResultSet rs = ps.executeQuery();
             while (rs.next()) {
 
-                Caixa caixa = new Caixa();
-                caixa.setRua(rs.getString("Rua"));
-                caixa.setEstante(rs.getString("Estante"));
-                caixa.setColuna(rs.getString("Coluna"));
-                caixa.setAndar(rs.getString("Andar"));
+                Armazem armazem = new Armazem();
+                armazem.setRua(rs.getString("Rua"));
+                armazem.setEstante(rs.getString("Estante"));
+                armazem.setColuna(rs.getString("Coluna"));
+                armazem.setAndar(rs.getString("Andar"));
 
-                lista.add(caixa);
+                lista.add(armazem);
             }
             rs.close();
             ps.close();
@@ -135,28 +136,28 @@ public class ArmazemDAO extends MySQL {
         return lista;
     }
 
-    public Caixa getCaixaById(int idCaixa) {
+    public Armazem getArmazemById(int idArmazem) {
         Connection c = this.getConnection();
-        Caixa caixa = null;
+        Armazem armazem = null;
         try {
-            PreparedStatement ps = c.prepareStatement("SELECT idCaixa, "
+            PreparedStatement ps = c.prepareStatement("SELECT idArmazem, "
                     + "rua, estante, coluna, andar "
-                    + "FROM caixa WHERE idCaixa = ?");
-            ps.setInt(1, idCaixa);
+                    + "FROM armazem WHERE idArmazem = ?");
+            ps.setInt(1, idArmazem);
             ResultSet rs = ps.executeQuery();
             while (rs.next()) {
 
-                caixa = new Caixa();
-                caixa.setIdCaixa(rs.getInt("idCaixa"));
-                caixa.setRua(rs.getString("Rua"));
-                caixa.setEstante(rs.getString("Estante"));
-                caixa.setColuna(rs.getString("Coluna"));
-                caixa.setAndar(rs.getString("Andar"));
+                armazem = new Armazem();
+                armazem.setIdArmazem(rs.getInt("idArmazem"));
+                armazem.setRua(rs.getString("Rua"));
+                armazem.setEstante(rs.getString("Estante"));
+                armazem.setColuna(rs.getString("Coluna"));
+                armazem.setAndar(rs.getString("Andar"));
 
             }
             rs.close();
             ps.close();
-            return caixa;
+            return armazem;
         } catch (SQLException ex) {
             ex.printStackTrace();
         } finally {

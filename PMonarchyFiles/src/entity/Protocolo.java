c6f01ebbb71d40a9d944dd5fd5c8ba93b503e409
VDISOO0219 <VDISOO0219@SNSOO1EP01VM103.saojose.sc.senai.br>
2015-06-30 21:52:00 -0300

    TelaProtocolo,ClasseProtocolo,DAOProtocolo

---
+++ PMonarchyFiles/src/entity/Protocolo.java
@@ -0,0 +1,119 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package entity;
+
+import java.util.Date;
+
+/**
+ *
+ * @author VDISOO0219
+ */
+public class Protocolo {
+    private int quantidadeDocumentos;
+    private String cpf;
+    private String responsavelEntrega;
+    private Empresa empresa;
+    private Usuario responsavelCadastro;
+    private Usuario responsavelSeparacao;
+    private Usuario responsavelEstocagem;
+    private TipoDocumento tipoDocumento;
+    private int tipoProtocolo;
+    private Date dataHora;
+    private int numeroProtocolo;
+
+    public int getQuantidadeDocumentos() {
+        return quantidadeDocumentos;
+    }
+
+    public void setQuantidadeDocumentos(int quantidadeDocumentos) {
+        this.quantidadeDocumentos = quantidadeDocumentos;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getResponsavelEntrega() {
+        return responsavelEntrega;
+    }
+
+    public void setResponsavelEntrega(String responsavelEntrega) {
+        this.responsavelEntrega = responsavelEntrega;
+    }
+
+    public Empresa getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(Empresa empresa) {
+        this.empresa = empresa;
+    }
+
+    public Usuario getResponsavelCadastro() {
+        return responsavelCadastro;
+    }
+
+    public void setResponsavelCadastro(Usuario responsavelCadastro) {
+        this.responsavelCadastro = responsavelCadastro;
+    }
+
+    public Usuario getResponsavelSeparacao() {
+        return responsavelSeparacao;
+    }
+
+    public void setResponsavelSeparacao(Usuario responsavelSeparacao) {
+        this.responsavelSeparacao = responsavelSeparacao;
+    }
+
+    public Usuario getResponsavelEstocagem() {
+        return responsavelEstocagem;
+    }
+
+    public void setResponsavelEstocagem(Usuario responsavelEstocagem) {
+        this.responsavelEstocagem = responsavelEstocagem;
+    }
+
+    public TipoDocumento getTipoDocumento() {
+        return tipoDocumento;
+    }
+
+    public void setTipoDocumento(TipoDocumento tipoDocumento) {
+        this.tipoDocumento = tipoDocumento;
+    }
+
+    public int getTipoProtocolo() {
+        return tipoProtocolo;
+    }
+
+    public void setTipoProtocolo(int tipoProtocolo) {
+        this.tipoProtocolo = tipoProtocolo;
+    }
+
+    public Date getDataHora() {
+        return dataHora;
+    }
+
+    public void setDataHora(Date dataHora) {
+        this.dataHora = dataHora;
+    }
+
+    
+    public int getNumeroProtocolo() {
+        return numeroProtocolo;
+    }
+
+    public void setNumeroProtocolo(int numeroProtocolo) {
+        this.numeroProtocolo = numeroProtocolo;
+    }
+
+ 
+    
+}

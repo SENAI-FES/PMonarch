f1e93c4f104e09d6ff22813b1db2c8243b3dca4f
matheus_santos_fraga <matheus_santos_fraga@SNSOO1EP01VM123.saojose.sc.senai.br>
2015-06-30 21:34:32 -0300

    sql status armazem

--- PMonarchyFiles/src/scripts/dbMonarchyFiles.sql
+++ PMonarchyFiles/src/scripts/dbMonarchyFiles.sql
@@ -21,7 +21,7 @@ USE `dbmonarchyfiles` ;
 DROP TABLE IF EXISTS `dbmonarchyfiles`.`armazem` ;
 
 CREATE TABLE IF NOT EXISTS `dbmonarchyfiles`.`armazem` (
-  `ativo` BOOLEAN NOT NULL,
+  `status` BOOLEAN NOT NULL,
   `rua` VARCHAR(100) NOT NULL,
   `estante` VARCHAR(100) NOT NULL,
   `coluna` VARCHAR(100) NOT NULL,

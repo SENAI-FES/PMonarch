ef040395333553640b6f9e81f90c0f249719864e
matheus_santos_fraga <matheus_santos_fraga@SNSOO1EP01VM133.saojose.sc.senai.br>
2015-07-03 19:42:12 -0300

    Telas copiadas para View

---
+++ PMonarchyFiles/src/view/TelaPrateleiraNovo.java
@@ -0,0 +1,279 @@
+package view;
+
+import Prototipos.*;
+import com.mysql.jdbc.exceptions.MySQLIntegrityConstraintViolationException;
+import dao.ArmazemDAO;
+import entity.Armazem;
+import javax.swing.JOptionPane;
+
+public class TelaPrateleiraNovo extends javax.swing.JDialog {
+
+    public TelaPrateleiraNovo(java.awt.Frame parent, boolean modal) {
+        super(parent, modal);
+        initComponents();
+        this.setLocationRelativeTo(null);
+        this.novo = true;
+    }
+
+    public TelaPrateleiraNovo(java.awt.Frame parent, boolean modal, boolean novo) {
+        super(parent, modal);
+        initComponents();
+        this.setLocationRelativeTo(null);
+        this.novo = novo;
+    }
+    public TelaPrateleiraNovo(java.awt.Frame parent, boolean modal, boolean novo, Armazem armazem) {
+        super(parent, modal);
+        initComponents();
+        this.setLocationRelativeTo(null);
+        this.novo = novo;
+        this.armazem = armazem;
+        this.armazemKey = armazem;
+        txtRua.setText(armazem.getRua());
+        txtAndar.setText(armazem.getAndar());
+        txtEstante.setText(armazem.getEstante());
+        txtColunaInicio.setText(armazem.getColuna());
+        txtColunaFim.setText(armazem.getColuna());
+        txtColunaFim.setEnabled(false);
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        txtAndar = new javax.swing.JTextField();
+        txtRua = new javax.swing.JTextField();
+        txtColunaInicio = new javax.swing.JFormattedTextField();
+        btnSalvar = new javax.swing.JButton();
+        btnCancelar = new javax.swing.JButton();
+        jLabel4 = new javax.swing.JLabel();
+        txtEstante = new javax.swing.JTextField();
+        txtColunaFim = new javax.swing.JFormattedTextField();
+        jLabel5 = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        setTitle("Cadastro de Prateleira");
+
+        jLabel1.setText("Rua:");
+
+        jLabel2.setText("Andar:");
+
+        jLabel3.setText("Coluna:");
+
+        txtColunaInicio.setFormatterFactory(new javax.swing.text.DefaultFormatterFactory(new javax.swing.text.NumberFormatter(java.text.NumberFormat.getIntegerInstance())));
+        txtColunaInicio.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyReleased(java.awt.event.KeyEvent evt) {
+                txtColunaInicioKeyReleased(evt);
+            }
+        });
+
+        btnSalvar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/OK.png"))); // NOI18N
+        btnSalvar.setText("Salvar");
+        btnSalvar.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        btnSalvar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnSalvarActionPerformed(evt);
+            }
+        });
+
+        btnCancelar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/Cancel.png"))); // NOI18N
+        btnCancelar.setText("Cancelar");
+        btnCancelar.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        btnCancelar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnCancelarActionPerformed(evt);
+            }
+        });
+
+        jLabel4.setText("Estante:");
+
+        txtColunaFim.setFormatterFactory(new javax.swing.text.DefaultFormatterFactory(new javax.swing.text.NumberFormatter(java.text.NumberFormat.getIntegerInstance())));
+
+        jLabel5.setText("até");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(103, 103, 103)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(btnSalvar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(btnCancelar))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jLabel3)
+                            .addComponent(jLabel2)
+                            .addComponent(jLabel1)
+                            .addComponent(jLabel4))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(txtAndar)
+                            .addComponent(txtRua)
+                            .addComponent(txtEstante)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(txtColunaInicio, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(jLabel5)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(txtColunaFim, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(0, 0, Short.MAX_VALUE)))
+                        .addGap(7, 7, 7)))
+                .addGap(89, 89, 89))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(29, 29, 29)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1)
+                    .addComponent(txtRua, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(txtEstante, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel4))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2)
+                    .addComponent(txtAndar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel3)
+                    .addComponent(txtColunaInicio, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(txtColunaFim, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel5))
+                .addGap(20, 20, 20)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnSalvar)
+                    .addComponent(btnCancelar))
+                .addGap(23, 23, 23))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void btnCancelarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCancelarActionPerformed
+        this.dispose();
+    }//GEN-LAST:event_btnCancelarActionPerformed
+
+    private void btnSalvarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSalvarActionPerformed
+        armazem = new Armazem();
+        armazem.setColunaInicio(txtColunaInicio.getText());
+        armazem.setColunaFim(txtColunaFim.getText());
+        int colunaInicio, colunaFim;
+        colunaInicio = Integer.parseInt(armazem.getColunaInicio());
+        colunaFim = Integer.parseInt(armazem.getColunaFim());
+        ArmazemDAO dao = new ArmazemDAO();
+        boolean sucesso = false;
+        if (colunaInicio < colunaFim
+                || colunaInicio == colunaFim) {
+            for (int i = colunaInicio; i <= colunaFim; i++) {
+                try {
+                    armazem = new Armazem();
+                    armazem.setAndar(txtAndar.getText());
+                    armazem.setRua(txtRua.getText());
+                    armazem.setEstante(txtEstante.getText());
+                    armazem.setColuna(i + "");
+                    if (novo) {
+                        armazem.setAtivo(true);
+                        if (dao.insert(armazem)) {
+                            sucesso = true;
+                        } else {
+                            sucesso = false;
+                            throw new MySQLIntegrityConstraintViolationException();
+                        }
+                    } else {
+                        if (dao.update(armazem, armazemKey)) { 
+                            sucesso = true;
+                        } else { 
+                            sucesso = false;
+                            throw new MySQLIntegrityConstraintViolationException();
+                        }
+                    }
+                } catch (MySQLIntegrityConstraintViolationException e) {
+                    JOptionPane.showMessageDialog(null, "A coluna " + i + " já existe");
+                } catch (NumberFormatException e) { 
+                    JOptionPane.showMessageDialog(null, "Preencha todos os campos.");
+                }
+            }
+        } else {
+            JOptionPane.showMessageDialog(null, "A coluna inicial deve ser menor que a coluna final");
+        }
+        if (sucesso) {
+            JOptionPane.showMessageDialog(null, "Prateleiras cadastrada com sucesso");
+        }
+        this.dispose();
+    }//GEN-LAST:event_btnSalvarActionPerformed
+
+    private void txtColunaInicioKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtColunaInicioKeyReleased
+        if (!novo) { 
+            txtColunaFim.setText(txtColunaInicio.getText());
+        }
+    }//GEN-LAST:event_txtColunaInicioKeyReleased
+
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(TelaPrateleiraNovo.class
+                    .getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(TelaPrateleiraNovo.class
+                    .getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(TelaPrateleiraNovo.class
+                    .getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(TelaPrateleiraNovo.class
+                    .getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the dialog */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                TelaPrateleiraNovo dialog = new TelaPrateleiraNovo(new javax.swing.JFrame(), true);
+                dialog.addWindowListener(new java.awt.event.WindowAdapter() {
+                    @Override
+                    public void windowClosing(java.awt.event.WindowEvent e) {
+                        System.exit(0);
+                    }
+                });
+                dialog.setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnCancelar;
+    private javax.swing.JButton btnSalvar;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JTextField txtAndar;
+    private javax.swing.JFormattedTextField txtColunaFim;
+    private javax.swing.JFormattedTextField txtColunaInicio;
+    private javax.swing.JTextField txtEstante;
+    private javax.swing.JTextField txtRua;
+    // End of variables declaration//GEN-END:variables
+    boolean novo;
+    Armazem armazem;
+    Armazem armazemKey;
+}
